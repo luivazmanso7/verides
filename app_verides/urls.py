756c916cd9fbce8642bd626a8458f25517af08e5
marceloh090 <149490670+marceloh090@users.noreply.github.com>
2024-10-17 23:38:52 -0300

    Update urls.py

--- app_verides/urls.py
+++ app_verides/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
     path('contribuir/', views.contribuir, name='contribuir'),
     path('inscricao/', views.inscricao, name='inscricao'),
     path('doacoes/', views.doacoes, name='doacoes'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('contato/', views.contato, name='contato'),
 ]
