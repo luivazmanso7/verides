0e7254058aa0f47ed75d70c4c260f397f5957cd9
João Marcelo87 <151652789+emerato@users.noreply.github.com>
2024-11-27 20:56:46 -0300

    Add files via upload

---
+++ Veridess/app_verides/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import home
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contribuir/', views.contribuir, name='contribuir'),
+    path('inscricao/', views.inscricao, name='inscricao'),
+    path('doacoes/', views.doacoes, name='doacoes'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('contato/', views.contato, name='contato'),
+    path('sobrenos/', views.sobrenos, name='sobrenos'),
+    path('confirmacao/', views.confirmacao, name='pagina_de_confirmacao'),
+    path('faq/', views.faq_page, name='faq_page'),
+]
